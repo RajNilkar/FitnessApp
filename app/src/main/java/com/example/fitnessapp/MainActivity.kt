88251834b279834a0248b401030eb9de449f2649
RajNilkar <rajitnilkar28@gmail.com>
2025-02-26 20:23:55 -0700

    login UI
    implemented a card view for better UI visualization and test for login authentication.

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.fitnessapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,28 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val usernameTextView= findViewById<EditText>(R.id.etvUsername)
+        val passwordTextView= findViewById<EditText>(R.id.etvPassword)
+        val loginButton= findViewById<Button>(R.id.btnLogin)
+
+        loginButton.setOnClickListener {
+            val username= usernameTextView.text.toString().trim()
+            val password= passwordTextView.text.toString().trim()
+            loginCredentials(username, password)
+        }
+    }
+
+    private fun loginCredentials(username: String, password: String)
+    {
+        if(username == "user" && password == "1234")
+        {
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+        }
+        else
+        {
+            Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
         }
     }
 }
