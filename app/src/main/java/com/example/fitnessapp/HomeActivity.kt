b5561909b24040d7f9bbb20b4edb29a51bba2952
Rubi Trejp <rubiceltrejo2@isu.edu>
2025-03-03 19:18:41 -0700

    Created homepage UI

---
+++ app/src/main/java/com/example/fitnessapp/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fitnessapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home) // This will be created in the next step
+    }
+
+}
