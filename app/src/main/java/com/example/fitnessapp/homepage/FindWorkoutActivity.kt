bedbc049c8cc68e0cb0506b160f364e94ca297c7
RajNilkar <rajitnilkar28@gmail.com>
2025-03-28 14:29:34 -0700

    fixed UI and display of exercises
    User can now select exercises based on the muscle group and now each exercise has a Log button next to it.

--- app/src/main/java/com/example/fitnessapp/homepage/FindWorkoutActivity.kt
+++ app/src/main/java/com/example/fitnessapp/homepage/FindWorkoutActivity.kt
@@ -1,6 +1,10 @@
 package com.example.fitnessapp.homepage
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +28,35 @@ class FindWorkoutActivity : AppCompatActivity() {
         recyclerView= findViewById(R.id.rclViewExercises)
         recyclerView.layoutManager= LinearLayoutManager(this)
 
+        val spinner= findViewById<Spinner>(R.id.spinnerMuscleGroups)
+        val muscleGroups= listOf(
+            "Select Muscle Group",
+            "abdominals", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats",
+            "lower_back", "upper_back", "quadriceps", "traps", "triceps"
+        )
+
+        val spinnerAdapter= ArrayAdapter(this, R.layout.item_spinner, muscleGroups)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter= spinnerAdapter
+
+        spinner.onItemSelectedListener= object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedMuscle= muscleGroups[position]
+                if(selectedMuscle != "Select Muscle Group"){
+                    viewModel.fetchExercises(selectedMuscle)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //Nothing is selected
+            }
+        }
+
         viewModel.listOfExercises.observe(this){exerciseList ->
             for(exercise in exerciseList)
             {
@@ -33,6 +66,5 @@ class FindWorkoutActivity : AppCompatActivity() {
             recyclerView.adapter= exercisesAdapter
         }
 
-        viewModel.fetchExercises("chest")
     }
 }
