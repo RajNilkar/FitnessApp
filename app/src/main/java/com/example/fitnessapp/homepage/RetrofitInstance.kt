537bb986835c4c968072cc6447f4f36683b06327
RajNilkar <rajitnilkar28@gmail.com>
2025-05-08 22:33:32 -0600

    Changed the api key
    needed to update the api key

--- app/src/main/java/com/example/fitnessapp/homepage/RetrofitInstance.kt
+++ app/src/main/java/com/example/fitnessapp/homepage/RetrofitInstance.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitInstance {
     companion object{
         private const val BASE_URL= "https://api.api-ninjas.com/v1/"
-        private const val API_KEY= "Tte7Hu0ulvG8UJ5CglfW9A==mXljHZlxuMMOe3Tp"
+        private const val API_KEY=  "LWSMI7K4IVzYbhunQap8wdkpMALmGyE1avkGXyfW" 
 
         val intercept= HttpLoggingInterceptor().apply {
             level= HttpLoggingInterceptor.Level.BODY
